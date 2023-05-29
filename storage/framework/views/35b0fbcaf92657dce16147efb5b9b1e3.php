896b43e49d4851f1e7e9d59d5e3c754f77b167d9
andreanara <97453582+andreanara@users.noreply.github.com>
2023-05-29 20:35:57 +0700

    Add files via upload

---
+++ storage/framework/views/35b0fbcaf92657dce16147efb5b9b1e3.php
@@ -0,0 +1,88 @@
+<?php $__env->startSection('content'); ?>
+    <div class="container-sm my-5">
+        <form action="<?php echo e(route('employees.store')); ?>" method="POST">
+            <?php echo csrf_field(); ?>
+            <div class="row justify-content-center">
+                <div class="p-5 bg-light rounded-3 border col-xl-6">
+                    <div class="mb-3 text-center">
+                        <i class="bi-person-circle fs-1"></i>
+                        <h4>Create Employee</h4>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="firstName" class="form-label">First Name</label>
+                            <input class="form-control <?php if($errors->has('firstName')): ?> is-invalid <?php endif; ?>" type="text"
+                                name="firstName" id="firstName" value="<?php echo e(old('firstName')); ?>"
+                                placeholder="Enter First Name">
+                            <?php if($errors->has('firstName')): ?>
+                                <small class="text-danger"><?php echo e($errors->first('firstName')); ?></small>
+                            <?php endif; ?>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="lastName" class="form-label">Last Name</label>
+                            <input class="form-control <?php if($errors->has('lastName')): ?> is-invalid <?php endif; ?>" type="text"
+                                name="lastName" id="lastName" value="<?php echo e(old('lastName')); ?>" placeholder="Enter Last Name">
+                            <?php if($errors->has('lastName')): ?>
+                                <small class="text-danger"><?php echo e($errors->first('lastName')); ?></small>
+                            <?php endif; ?>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input class="form-control <?php if($errors->has('lastName')): ?> is-invalid <?php endif; ?>" type="text"
+                                name="email" id="email" value="<?php echo e(old('email')); ?>" placeholder="Enter Email">
+                            <?php if($errors->has('email')): ?>
+                                <small class="text-danger"><?php echo e($errors->first('email')); ?></small>
+                            <?php endif; ?>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="age" class="form-label">Age</label>
+                            <input class="form-control <?php if($errors->has('age')): ?> is-invalid <?php endif; ?>" type="text"
+                                name="age" id="age" value="<?php echo e(old('age')); ?>" placeholder="Enter Age">
+                            <?php if($errors->has('age')): ?>
+                                <small class="text-danger"><?php echo e($errors->first('age')); ?></small>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="position" class="form-label">Position</label>
+                        <select name="position" id="position" class="form-select">
+                            <?php $__currentLoopData = $positions; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $position): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <option value="<?php echo e($position->id); ?>"
+                                    <?php echo e(old('position') == $position->id ? 'selected' : ''); ?>>
+                                    <?php echo e($position->code . ' - ' . $position->name); ?></option>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </select>
+                        <?php $__errorArgs = ['position'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <div class="text-danger"><small><?php echo e($message); ?></small></div>
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-6 d-grid">
+                            <a href="<?php echo e(route('employees.index')); ?>" class="btn btn-outline-dark btn-lg mt-3">
+                                <i class="bi-arrow-left-circle me-2"></i> Cancel</a>
+                        </div>
+                        <div class="col-md-6 d-grid">
+                            <button type="submit" class="btn btn-dark btn-lg mt-3"><i class="bi-check-circle me-2"></i>
+                                Save</button>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </form>
+    </div>
+<?php $__env->stopSection(); ?>
+
+
+
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Dell\Documents\TUGAS KULIAH\SMT 4\Pemrograman Framework\Praktikum 4\laravelController\resources\views/employee/create.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/74f0b1cabfcd03d9ad624821ca961e90.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="d-flex">
+        <a href="<?php echo e(route('employees.show', ['employee' => $employee->id])); ?>" class="btn btn-outline-dark btn-sm me-2">
+            <i class="bi-person-lines-fill"></i></a>
+        <a href="<?php echo e(route('employees.edit', ['employee' => $employee->id])); ?>" class="btn btn-outline-dark btn-sm me-2">
+            <i class="bi-pencil-square"></i></a>
+
+        <div>
+            <form action="<?php echo e(route('employees.destroy', ['employee' => $employee->id])); ?>" method="POST">
+                <?php echo csrf_field(); ?>
+                <?php echo method_field('delete'); ?>
+                <button type="submit" class="btn btn-outline-dark btn-sm me-2"><i class="bi-trash"></i></button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
+
+<?php /**PATH C:\Users\Lenovo\Documents\KULIAH\semester 4\pemrograman framework\praktikum6\resources\views/employee/actions.blade.php ENDPATH**/ ?>
